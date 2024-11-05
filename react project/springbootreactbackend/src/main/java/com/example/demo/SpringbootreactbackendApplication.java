8c87e7f32e7a82d64a949cc8e54033ea5fd4af4f
DLR2204 <n130488@rguktn.ac.in>
2024-11-05 22:34:12 +0530

    Add files via upload

---
+++ react project/springbootreactbackend/src/main/java/com/example/demo/SpringbootreactbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootreactbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootreactbackendApplication.class, args);
+	}
+
+}
